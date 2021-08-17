e9aaec5f76d2454c3a8b871075849de51160bcfd
momoxstarx <momo.star@gmail.com>
2021-08-17 10:55:51 -0700

    color bung

--- my-app/src/components/OneBung.js
+++ my-app/src/components/OneBung.js
@@ -4,25 +4,37 @@ import { View, Text, TextInput } from 'react-native';
 
 const OneBung = () => {
 
-  const [price, onChangePrice] = useState(0.0);
-  const [qty, onChangeQty] = useState(1);
+  const [price, onChangePrice] = useState(null);
+  const [qty, onChangeQty] = useState(null);
+
+  const calc = () => {
+    return (price / qty).toFixed(2)
+  }
 
   return (
     <View style={styles.bung}>
       <Text style={styles.bungText}>Description</Text>
-      <Text style={styles.bungText}>Price $:</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangePrice}
-        value={price}
-      />
-      <Text style={styles.bungText}>Qty #:</Text>
-      <TextInput
-        style={styles.input}
-        onChangeQty={onChangeQty}
-        value={qty}
-      />
-      <Text>{price}</Text>
+      <View style={styles.bungRow}>
+        <Text style={styles.bungText}>Price $:</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          onChangeText={onChangePrice}
+          value={price}
+          placeholder="Enter price"
+        />
+      </View>
+      <View style={styles.bungRow}>
+        <Text style={styles.bungText}>Qty #:</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          onChangeText={onChangeQty}
+          value={qty}
+          placeholder="Enter qty"
+        />
+      </View>
+      <Text style={styles.bungText}>$/#: {price && qty ? calc() : price}</Text>
     </View>
   );
 };
--- my-app/src/screens/BungScreen.js
+++ my-app/src/screens/BungScreen.js
@@ -2,20 +2,20 @@ import React from "react";
 import { useState } from "react";
 import styles from "../static/styles";
 import { View, Text, TextInput } from "react-native";
-import BungView from "../components/BungView"
+import OneBung from "../components/OneBung"
 
 const BungScreen = ({ navigation, route }) => {
   const [bungs, setBungs] = useState([]);
 
   return (
     <View style={styles.bungContainer}>
-      <BungView />
-      <BungView />
-      <BungView />
-      <BungView />
-      <BungView />
-      <BungView />
-      <BungView />
+      <OneBung />
+      <OneBung />
+      <OneBung />
+      <OneBung />
+      <OneBung />
+      <OneBung />
+      
     </View>
   );
 };
