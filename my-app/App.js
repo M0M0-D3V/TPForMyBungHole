c861a4354586e6efd4d7de53ba85527f0164dc95
momoxstarx <momo.star@gmail.com>
2023-04-18 15:04:42 -0700

    update

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "react-native-gesture-handler";
+import { initializeApp } from 'firebase/app';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from './src/screens/HomeScreen'
 import BungScreen from './src/screens/BungScreen'
 
 const Stack = createStackNavigator();
+
+const Aapp = initializeApp(firebaseConfig);
 
 const App = () => {
   return (
