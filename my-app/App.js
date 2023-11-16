54f4c4eaf49fb806184129cebf812d5df00ab7a7
Monica Hong <momo.star@gmail.com>
2023-11-16 05:51:06 -0800

    update expo and add firebase

--- my-app/App.js
+++ my-app/App.js
@@ -1,31 +1,29 @@
 import React from "react";
 import "react-native-gesture-handler";
-import { initializeApp } from 'firebase/app';
+import firebase from "expo-firebase-app";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from './src/screens/HomeScreen'
-import BungScreen from './src/screens/BungScreen'
+import HomeScreen from "./src/screens/HomeScreen";
+import BungScreen from "./src/screens/BungScreen";
 
 const Stack = createStackNavigator();
 
-const Aapp = initializeApp(firebaseConfig);
+const firebaseConfig = {};
+
+// initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
-        <Stack.Screen
-          name="Bungholio"
-          component={BungScreen}
-        />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Bungholio" component={BungScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
 
 export default App;
