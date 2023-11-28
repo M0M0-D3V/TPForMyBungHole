9a589069b24da1f21b5ab361e106e5cda1465e15
Monica Hong <momo.star@gmail.com>
2023-11-28 09:10:33 -0800

    firebase config and tsx placeholder

--- my-app/App.js
+++ my-app/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "react-native-gesture-handler";
-import firebase from "expo-firebase-app";
+import firebase from "firebase/app";
+import "firebase/database";
+import { firebaseConfig } from "./firebaseConfig";
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+// import firebase from "expo-firebase-app";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/screens/HomeScreen";
@@ -8,12 +17,14 @@ import BungScreen from "./src/screens/BungScreen";
 
 const Stack = createStackNavigator();
 
-const firebaseConfig = {};
-
 // initialize Firebase
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
+
+// const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
 
 const App = () => {
   return (
---
+++ my-app/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyCAAZtr6MVDcZyXHCiPPGA81jkpwGJn3Lk",
+  authDomain: "tpformybunghole-iamcornholio.firebaseapp.com",
+  databaseURL: "https://tpformybunghole-iamcornholio.firebaseio.com",
+  projectId: "tpformybunghole-iamcornholio",
+  storageBucket: "tpformybunghole-iamcornholio.appspot.com",
+  messagingSenderId: "851031084045",
+  // appId: "app-id",
+  // measurementId: "G-measurement-id",
+};
